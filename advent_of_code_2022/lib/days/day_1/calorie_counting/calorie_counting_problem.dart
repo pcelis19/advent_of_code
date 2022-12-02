3ef3b995ce72ed963640875195b1461dba69c8d2
Pedro Celis <pcelis19@gmail.com>
2022-12-01 23:33:32 -0800

    wrote up algorithm, and injected repo

---
+++ advent_of_code_2022/lib/days/day_1/calorie_counting/calorie_counting_problem.dart
@@ -0,0 +1,39 @@
+import 'package:advent_of_code_2022/days/day_1/repo/io_calorie_counting_repo.dart';
+import 'package:advent_of_code_2022/lib.dart';
+
+class CalorieCounting extends Problem<int> {
+  final ICalorieCountingRepo _repo;
+
+  const CalorieCounting({
+    ICalorieCountingRepo? repo,
+  }) : _repo = repo ?? const IOCalorieCountingRepo();
+
+  @override
+  Future<int> solution() async {
+    final input = await _repo.getInput();
+    final solution = _highestCalorieCount(input);
+    return solution;
+  }
+
+  int _highestCalorieCount(List<String> input) {
+    final iterator = input.iterator;
+    int currentMax = -1;
+    int currentTally = 0;
+    while (iterator.moveNext()) {
+      final current = iterator.current;
+      if (current.isEmpty) {
+        if (currentTally > currentMax) {
+          currentMax = currentTally;
+        }
+        currentTally = 0;
+      } else {
+        int value = int.parse(current);
+        currentTally += value;
+      }
+    }
+    if (currentTally > currentMax) {
+      currentMax = currentTally;
+    }
+    return currentMax;
+  }
+}
