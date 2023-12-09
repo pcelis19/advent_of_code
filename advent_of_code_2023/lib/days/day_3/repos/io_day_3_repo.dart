910189ecd13fae1403353d31c0c6acd4c43183ff
Pedro Celis <34390914+pcelis19@users.noreply.github.com>
2023-12-08 23:08:05 -0800

    Update io_day_3_repo.dart

--- advent_of_code_2023/lib/days/day_3/repos/io_day_3_repo.dart
+++ advent_of_code_2023/lib/days/day_3/repos/io_day_3_repo.dart
@@ -13,3 +13,24 @@ class IODay3Repo extends Day3Repo {
     return rows;
   }
 }
+
+class TestDay3Repo extends Day3Repo {
+  @override
+  Future<List<Day3PartAInputRow>> getPartA() async {
+    final number = Day3PartAInputNodeNumber(value: '1');
+    final symbol = Day3PartAInputNodeSymbol('*');
+    return [
+      Day3PartAInputRow(
+        xLocAndNode: {
+          0: symbol,
+          1: number,
+        },
+      ),
+      Day3PartAInputRow(
+        xLocAndNode: {
+          15: Day3PartAInputNodeNumber(value: '15'),
+        },
+      ),
+    ];
+  }
+}
