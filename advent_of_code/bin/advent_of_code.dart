9d670dc994725af50dda59ac758d4ce888724489
Pedro Celis <34390914+pcelis19@users.noreply.github.com>
2026-01-01 23:27:15 -0800

    created advent of code project

---
+++ advent_of_code/bin/advent_of_code.dart
@@ -0,0 +1,5 @@
+import 'package:advent_of_code/advent_of_code.dart' as advent_of_code;
+
+void main(List<String> arguments) {
+  print('Hello world: ${advent_of_code.calculate()}!');
+}
---
+++ advent_of_code/test/advent_of_code_test.dart
@@ -0,0 +1,8 @@
+import 'package:advent_of_code/advent_of_code.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
